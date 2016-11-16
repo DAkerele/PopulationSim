c5f14921268d8b91ff60cc39397bf5e742e93637
DAkerele <duardoakerele@gmail.com>
2016-11-15 21:14:56 -0500

    Add files via upload

---
+++ Pop Sim/Node.js
@@ -0,0 +1,40 @@
+"use strict";
+class Node{
+
+	 constructor(selected,posX,posY,Color,NodeNum){
+		this.isSelected = selected;
+		this.CoordX = posX;
+		this.CoordY = posY;
+		this.NodeNum = NodeNum;
+		this.Color = Color;
+
+	
+	}
+
+
+	setPosX(pos){
+
+		 centerX = pos;
+
+		}
+
+	setPosY(pos){
+
+		 	enterY = pos;
+
+		}
+
+	getPosX() {
+			
+			return this;
+
+		}
+
+	getPosY() {
+
+			return centerY;
+
+		}
+
+
+}
