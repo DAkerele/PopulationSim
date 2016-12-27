ca4601063aebbb5a2e508f154358ae792a020f67
DAkerele <duardoakerele@gmail.com>
2016-12-27 08:52:33 -0500

    Update Node.js

--- Pop Sim/Node.js
+++ Pop Sim/Node.js
@@ -1,12 +1,20 @@
 "use strict";
 class Node{
 
-	 constructor(selected,posX,posY,Color,NodeNum){
+	 constructor(selected,posX,posY,Color,NodeNum,startPer,genNum,startPop,plusplusS,plusminusS,minusminusS){
 		this.isSelected = selected;
 		this.CoordX = posX;
 		this.CoordY = posY;
 		this.NodeNum = NodeNum;
 		this.Color = Color;
+		this.startPer = startPer;
+		this.genNum = genNum;
+		this.startPop = startPop;
+		this.plusplusS = plusplusS;
+		this.plusminusS = plusminusS;
+		this.minusminusS = minusminusS;
+
+
 
 	
 	}
